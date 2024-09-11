73325cb66a55318951fe8391901d02ea45393060
sabrinacouto <sabrinacoutolima@gmail.com>
2024-09-11 19:59:42 -0300

    Atualiza a configuração inicial do app com Native Base

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NativeBaseProvider, Box, Text, Center } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1} bg="white">
+        <Box>
+          <Text fontSize="2xl" color="primary.500" fontWeight="bold" fontFamily="Roboto">
+            Olá TDSZ, nosso app está configurado com Native Base!
+          </Text>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
  
